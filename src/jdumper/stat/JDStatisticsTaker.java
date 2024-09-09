6fe6fbdf1bba60d4a4a457399deb0981016873dc
aayush_xid_su <ayushxidsu@gmail.com>
2024-09-09 11:47:40 +0530

    finished

---
+++ src/jdumper/stat/JDStatisticsTaker.java
@@ -0,0 +1,28 @@
+package jdumper.stat;
+import java.util.List;
+import java.util.Vector;
+import jpcap.packet.Packet;
+
+public abstract class JDStatisticsTaker
+{
+	public abstract String getName();
+
+	public abstract void analyze(List<Packet> packets);
+	public abstract void addPacket(Packet p);
+	
+	public abstract String[] getLabels();
+	public abstract String[] getStatTypes();
+	public abstract long[] getValues(int index);
+	
+	public abstract void clear();
+	
+	public JDStatisticsTaker newInstance(){
+		try{
+			return (JDStatisticsTaker)this.getClass().newInstance();
+		}catch(Exception e){
+			return null;
+		}
+	}
+}
+// copyright  belongs to aayush_xid_su 
+// all rights reserved aayush-xid-su
